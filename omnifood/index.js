e00dedcdf899a74fdbeea602b3284a74db766c9a
rohan23 <rohan@socialpilot.co>
2020-08-16 13:08:16 +0530

    added meta tag favicon and compated course

---
+++ omnifood/index.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+  "use strict";
+
+  var c,
+    currentScrollTop = 0,
+    navbar = $("nav");
+
+  $(window).scroll(function () {
+    var a = $(window).scrollTop();
+    var b = navbar.height();
+
+    currentScrollTop = a;
+
+    if (c < currentScrollTop && a > b + b) {
+      navbar.addClass("scrollUp");
+    } else if (c > currentScrollTop && !(a <= b)) {
+      navbar.removeClass("scrollUp");
+    }
+    c = currentScrollTop;
+  });
+});
+
+// mobile nav
+
+// $(".mobile-nav-icon").click();
+
+var isOpen = false;
+document.querySelector(".mobile-nav-icon").addEventListener("click", (e) => {
+  e.preventDefault();
+
+  $(".main-nav").css("display", !isOpen ? "inline-block" : "none");
+  isOpen = !isOpen;
+});
